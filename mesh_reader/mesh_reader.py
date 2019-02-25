55d9e3ac0977da3b480dbdc9faef8ee2dde6870a
viacheslav <viacheslav_plekhanov@epam.com>
2019-02-25 15:54:44 +0300

    add new functionality

--- mesh_reader/mesh_reader.py
+++ mesh_reader/mesh_reader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.5
+#!/usr/bin/python3.6
 import re
 from argparse import ArgumentParser
 import json
--- million_dataset_analyzer/million_dataset_analyzer.py
+++ million_dataset_analyzer/million_dataset_analyzer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.5
+#!/usr/bin/python3.6
 import json
 import re
 
@@ -9,7 +9,10 @@ data_len = 0
 terms = {}
 r_terms = {}
 entries = {}
-INDEX_FOR_RAW_DATA = 'letterAbstract'
+INDEX_FOR_RAW_DATA_ABSTRACT = 'letterAbstract'
+INDEX_FOR_RAW_DATA_TITLE = 'title'
+INDEX_FOR_RAW_DATA_KEYWORDS = 'keyWords'
+
 INDEX_FOR_PROCESSED_DATA = 'letterAbstract_processed'
 INDEX_2 = 'all_features'
 p_entries = {}
@@ -56,21 +59,34 @@ def get_data_pairs(data):
 
 def get_data_words(data):
     global data_len
+    import re
     data_len += len(data)
-    return [({term for term in article[INDEX_2]}, article['id']) for article in data]
+    res = []
+    for article in data:
+        terms_set = set()
+        terms_set = terms_set.union(
+            set(filter(lambda x: len(x) > 1, re.split('\\W+', article[INDEX_FOR_RAW_DATA_ABSTRACT]))))
+        terms_set = terms_set.union(
+            set(filter(lambda x: len(x) > 1, re.split('\\W+', article[INDEX_FOR_RAW_DATA_TITLE]))))
+        terms_set = terms_set.union(article[INDEX_FOR_RAW_DATA_KEYWORDS])
+        terms_set = set(map(str.lower, terms_set))
+        res += [(terms_set, article['id'])]
+    return res
+    # return [({term for term in article[INDEX_2]}, article['id']) for article in data]
 
 
 def compute(data):
     global r_terms, total, maximum, minimum, zeros, no_mesh_words
     for article, a_id in data:
+        if a_id == 105230:
+            print(article)
         count = 0
         for term in article:
-            if term in r_terms or term in r_p_entries:
+            # if term in r_terms or term in r_p_entries:
+            if term in terms or term in entries:
+                if a_id == 105230:
+                    print(term)
                 count += 1
-        if count == 0:
-            if a_id in no_mesh_words:
-                print(a_id)
-            zeros += 1
         total += count
         if count > maximum:
             maximum = count
@@ -80,43 +96,36 @@ def compute(data):
 
 def main():
     init_terms_maps()
-    global total, terms, r_terms, minimum, maximum, r_p_entries, p_entries, zeros, no_mesh_words
+    global total, terms, r_terms, minimum, maximum, r_p_entries, p_entries, zeros, entries
+    terms = {key.lower().strip().replace(',', ''): value for key, value in terms.items()}
+    entries = {key.lower().strip().replace(',', ''): value for key, value in entries.items()}
 
-    no_mesh_words = {
-        32947044,
-        22408454,
-        29136075,
-        29002729,
-        35605507,
-        26842075,
-        23551529,
-        23463712,
-        24104293,
-        28197861
-    }
-
-
-    # files_arr = ['../../small_data/part-00000-a2d14103-4a3f-44b2-bf68-f554c79953ff-c000.json']
+    # files_arr = ['/home/viacheslav/Documents/bbp/smallData/processed/part-00000-898980c5-9f2a-44f8-b0b1-492b05ae4e61-c000.json']
     files_arr = [
-        "../../million/processed/part-00000-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00001-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00002-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00003-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00004-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00005-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00006-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00007-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00008-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00009-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00010-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00011-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
-        "../../million/processed/part-00012-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json"
-    ]
-
+        '/home/viacheslav/Documents/bbp/smallData/json/part-00000-53ca1f94-9474-4995-b2f9-4f61f884a53e-c000.json',
+        '/home/viacheslav/Documents/bbp/smallData/json/part-00001-53ca1f94-9474-4995-b2f9-4f61f884a53e-c000.json',
+        '/home/viacheslav/Documents/bbp/smallData/json/part-00002-53ca1f94-9474-4995-b2f9-4f61f884a53e-c000.json',
+        '/home/viacheslav/Documents/bbp/smallData/json/part-00003-53ca1f94-9474-4995-b2f9-4f61f884a53e-c000.json', ]
+    # files_arr = [
+    #     "../../million/processed/part-00000-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00001-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00002-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00003-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00004-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00005-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00006-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00007-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00008-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00009-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00010-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00011-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json",
+    #     "../../million/processed/part-00012-792100ba-67d5-45dc-bb11-b7790fe70fdd-c000.json"
+    # ]
+    [print(term) for term in set(terms.keys()).union(set(entries.keys())) if len(term.split()) > 10]
     for file in files_arr:
-        compute(get_data_pairs(process_file(file)))
-
-    print(len(entries))
+        compute(get_data_words(process_file(file)))
+    # print(r_terms[r_p_entries['economic']])
+    # print(r_terms['economic'])
     print('total = ' + str(total))
     print('articles count = ' + str(data_len))
     print('average = ' + str(total / data_len))
